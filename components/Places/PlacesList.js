fddcac9fc07b63a78eedcd51986e37cfda97b291
manas5996 <manas5996@gmail.com>
2022-12-05 19:26:56 +0530

    After adding an Add Place screen and navigation

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FlatList, StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import PlaceItem from "./PlaceItem";
 
 const PlacesList = ({ places }) => {
