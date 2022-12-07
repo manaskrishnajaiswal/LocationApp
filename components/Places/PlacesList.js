8789cf3046f80d81cee34bf1e6ea3f05d3582c9f
manas5996 <manas5996@gmail.com>
2022-12-07 18:51:22 +0530

    After global colors and styling

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
+import { Colors } from "../../constants/colors";
 import PlaceItem from "./PlaceItem";
 
 const PlacesList = ({ places }) => {
@@ -30,5 +31,6 @@ const styles = StyleSheet.create({
   },
   fallbackText: {
     fontSize: 16,
+    color: Colors.primary200,
   },
 });
