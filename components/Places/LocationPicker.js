fcc943abb40d60f6539541b5530cd0faa4910621
manas5996 <manas5996@gmail.com>
2022-12-16 08:41:26 +0530

    After locating users

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -1,17 +1,52 @@
 import React from "react";
 import { Alert, Button, Image, StyleSheet, Text, View } from "react-native";
 import { Colors } from "../../constants/colors";
+import {
+  getCurrentPositionAsync,
+  PermissionStatus,
+  useForegroundPermissions,
+} from "expo-location";
 import OutlinedButton from "../UI/OutlinedButton";
 
 const LocationPicker = () => {
-  function getLocationHandler() {}
+  const [locationPermissionInformation, requestPermission] =
+    useForegroundPermissions();
+  async function verifyPermissions() {
+    if (
+      locationPermissionInformation.status === PermissionStatus.UNDETERMINED
+    ) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant location permissions to use this app."
+      );
+      return false;
+    }
+    return true;
+  }
+
+  async function getLocationHandler() {
+    const hasPermission = await verifyPermissions();
+    if (!hasPermission) {
+      return;
+    }
+    const location = await getCurrentPositionAsync();
+    console.log(location);
+  }
   function pickOnMapHandler() {}
   return (
     <View>
       <View style={styles.mapPreview}></View>
       <View style={styles.actions}>
-        <OutlinedButton icon="location">Locate User</OutlinedButton>
-        <OutlinedButton icon="map">Pick on Map</OutlinedButton>
+        <OutlinedButton icon="location" onPress={getLocationHandler}>
+          Locate User
+        </OutlinedButton>
+        <OutlinedButton icon="map" onPress={pickOnMapHandler}>
+          Pick on Map
+        </OutlinedButton>
       </View>
     </View>
   );
