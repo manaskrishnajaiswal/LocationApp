a685914de96e0018a8d72b93ea97d0d9f4d6e3e1
manas5996 <manas5996@gmail.com>
2022-12-19 13:31:21 +0530

    After fetching place detail data from the database

--- screens/PlaceDetails.js
+++ screens/PlaceDetails.js
@@ -1,20 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import OutlinedButton from "../components/UI/OutlinedButton";
 import { Colors } from "../constants/colors";
+import { fetchPlaceDetails } from "../util/database";
 
-const PlaceDetails = ({ route }) => {
+const PlaceDetails = ({ route, navigation }) => {
+  const [fetchedPlace, setFetchedPlace] = useState();
   function showOnMapHandler() {}
   const selectedPlaceId = route.params.placeId;
+
   useEffect(() => {
     // use selectedPalaceId for fetching data for a single place
+    async function loadPlaceData() {
+      const place = await fetchPlaceDetails(selectedPlaceId);
+      setFetchedPlace(place);
+
+      navigation.setOptions({
+        title: place.title,
+      });
+    }
+    loadPlaceData();
   }, [selectedPlaceId]);
+  //   console.log(fetchedPlace);
+  if (!fetchedPlace) {
+    return (
+      <View style={styles.fallback}>
+        <Text>Loading place data</Text>
+      </View>
+    );
+  }
   return (
     <ScrollView>
-      <Image style={styles.image} />
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={{ uri: fetchedPlace.imageUri }} />
+      </View>
       <View style={styles.locationContainer}>
         <View style={styles.addressContainer}>
-          <Text style={styles.address}>ADDRESS</Text>
+          <Text style={styles.address}>{fetchedPlace.address}</Text>
         </View>
         <OutlinedButton icon="map" onPress={showOnMapHandler}>
           View On Map
@@ -27,12 +49,21 @@ const PlaceDetails = ({ route }) => {
 export default PlaceDetails;
 
 const styles = StyleSheet.create({
+  fallback: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    margin: 24,
+  },
   image: {
     height: "35%",
     minHeight: 300,
     width: "100%",
   },
   locationContainer: {
+    marginTop: 96,
     justifyContent: "center",
     alignItems: "center",
   },
